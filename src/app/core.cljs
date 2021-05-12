9891c4293c9c290b883536931cdee8885bd13e6d
Oliver Runge <oliver.runge@gmail.com>
2021-05-12 14:33:51 +0200

    Add a line that fails and might provide a useful stacktrace

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -2,5 +2,11 @@
 
 (defn main []
   (let [c (.. js/document (createElement "DIV"))]
+    (str
+     (clojure.string/replace nil "" "")
+     (clojure.string/replace "foo" "f" "b")
+     (clojure.string/replace "bar" "b" "f"))
+
     (aset c "innerHTML" "<p>i'm dynamically created</p>")
     (.. js/document (getElementById "container") (appendChild c))))
+
